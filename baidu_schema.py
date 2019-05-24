e5eefd8f2f6e19b1eb42eb735557d024e6c81487
yan <yan306@basecare.cn>
2019-05-24 14:50:10 +0800

    update import

--- baidu_schema.py
+++ baidu_schema.py
@@ -1,15 +1,17 @@
 #coding = utf-8
-from urllib import quote,urlopen
+from urllib.parse import quote
+from urllib.request import urlopen
 from lxml import etree
 import pymongo
 conn = pymongo.MongoClient()
 
 def init_seed(seed_path):#初始化种子词
     seed_dict = {}
-    for line in open('seed_category.txt'):
+    #for line in open('seed_category.txt'):
+    for line in open(seed_path,encoding='utf-8'):
         line = line.strip().split(',')
-        category = line[0]
-        par_category = line[1]
+        category = line[0]          # 类别
+        par_category = line[1]         # 父类
         seed_dict[category] = par_category
 
     return seed_dict
@@ -18,6 +20,9 @@ def search_main(word):#搜索主函数
     url = 'http://baike.baidu.com/fenlei/%s'%quote(word)
     content = urlopen(url).read()
     sub_list,brother_list = collect_schema(content)
+
+    # f1 = open('nodes.csv', 'w')
+    # f2 = open('relations.csv', 'w')
     if not sub_list:
         return 
     else:
@@ -28,7 +33,8 @@ def search_main(word):#搜索主函数
                 data['sub_concept'] = sub_word
                 data['brother_concept'] = sub_list
                 conn['concept_project']['baidu_concept'].insert(data)
-                search_main(sub_word.encode('utf-8'))
+                search_main(sub_word.encode('utf-8'))         ##  递归遍历
+
 
 def collect_schema(content): #上下位抽取主函数
     selector = etree.HTML(content)
@@ -38,8 +44,8 @@ def collect_schema(content): #上下位抽取主函数
     brother_list = []
 
     if len(schema_list) == 2:
-        sub = schema_list[0]
-        brother = schema_list[1]
+        sub = schema_list[0]            ## 子类
+        brother = schema_list[1]        ## 相关分类
         sub_list = sub.xpath('./a/text()')
         brother_list = brother.xpath('./a/text()')
         
@@ -49,11 +55,11 @@ def collect_schema(content): #上下位抽取主函数
 
     return sub_list,brother_list
 
-
+from tqdm import tqdm
 def main():
     seed_path = './seed_baike_category.txt'
     seed_dict = init_seed(seed_path)
-    for seed,seed_par in seed_dict.items():
+    for seed,seed_par in tqdm(seed_dict.items()):
         search_main(seed)
 
 main()
